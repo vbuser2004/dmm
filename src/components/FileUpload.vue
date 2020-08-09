6ef8769a96fff1ec49c6d9af1398288a47730737
vbuser2004 <jeff@pitney.com>
2020-08-08 22:35:23 -0400

    Get File List

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,39 +1,41 @@
 <template>
   <v-container>
     <v-container>
-      <vue-dropzone
-        ref="myVueDropzone"
-        id="dropzone"
-        :options="dropzoneOptions"
-      ></vue-dropzone>
+      <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
     </v-container>
     <v-container>
-      <v-btn class="ma-2" outlined color="indigo">Outlined Button</v-btn>
+      <v-btn class="ma-2" @click="somemethod" outlined color="indigo">Get File List</v-btn>
     </v-container>
   </v-container>
 </template>
 
 <script>
-import vue2Dropzone from 'vue2-dropzone';
-import 'vue2-dropzone/dist/vue2Dropzone.min.css';
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     vueDropzone: vue2Dropzone,
   },
-  data: function() {
+  data: function () {
     return {
       dropzoneOptions: {
-        url: 'http://httpbin.org/post',
+        url: "http://httpbin.org/post",
         thumbnailWidth: 150,
-        dictDefaultMessage: 'ADD FILES',
+        dictDefaultMessage: "ADD FILES",
         autoProcessQueue: false,
         maxFilesize: 20,
         addRemoveLinks: true,
-        headers: { 'My-Awesome-Header': 'header value' },
+        headers: { "My-Awesome-Header": "header value" },
       },
     };
+  },
+  methods: {
+    somemethod: function () {
+      let fileList = this.$refs.myVueDropzone.getQueuedFiles();
+      console.log("Files: " + JSON.stringify(fileList));
+    },
   },
 };
 </script>
